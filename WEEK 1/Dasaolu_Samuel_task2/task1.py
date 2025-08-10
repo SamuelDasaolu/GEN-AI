fe0f03b8da5ad479d23ef311801af87d398e5c9a
SDasaolu <samueldasaolu40@gmail.com>
2025-08-10 19:02:48 +0100

    Week 1 Task Completed

---
+++ WEEK 1/Dasaolu_Samuel_task2/task1.py
@@ -0,0 +1,5 @@
+# Task 1
+print("This is Task 1")
+name = input("What is your Name? ")
+age = input("What is your Age? ")
+print(f"Welcome {name}, you are {age} years old")
---
+++ WEEK 1/Dasaolu_Samuel_task2/task7.py
@@ -0,0 +1,6 @@
+# Task 7
+print("This is Task 7")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in meters: "))
+name = input("Enter your name: ")
+print(f"My name is {name}, I am {age} years old, and my height is {height} meters.")
