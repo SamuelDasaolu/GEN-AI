d038b287837caeb62a7b831a411e8054e144b8a1
SDasaolu <samueldasaolu40@gmail.com>
2025-08-10 19:02:48 +0100

    Week 1 Task Completed

---
+++ WEEK 1/Dasaolu_Samuel_task2/task8.py
@@ -0,0 +1,6 @@
+# Task 8
+print("This is Task 8")
+distance_km = float(input("Enter the distance in km: "))
+fare_per_km = float(input("Enter the fare per km: "))
+total_fare = distance_km * fare_per_km
+print(f"The total fare is: NGN{total_fare:.2f}")
