d90cfe066ac26e3fdfc2ec1dc71a8c59945c704a
SDasaolu <samueldasaolu40@gmail.com>
2025-08-11 15:33:35 +0100

    WEEK 2, DAY 1

---
+++ WEEK 2/task3.py
@@ -0,0 +1,25 @@
+"""
+Task3: Pattern Matching & Splitting
+11. Given "apple,banana,orange", split the string into a list of fruits.
+
+12. Ask the user for a sentence and print each word on a new line.
+
+13. Replace all spaces in a string with underscores (_).
+
+14. Count how many times the letter "a" appears in "Banana".
+
+15. Check if a given string starts with "https://".
+"""
+
+# # Task 11
+# fruit_string = "apple,banana,orange"
+# fruit_list = fruit_string.split(',')
+# print(f"Fruit list: {fruit_list}")
+
+# # Task 12
+# user_sentence = input("Please input a sentence: ")
+# sentence_list = user_sentence.split(" ")
+# print(' \n'.join(sentence_list))
+
+# Task 13
+
---
+++ WEEK 2/working_with_strings.py
@@ -0,0 +1,22 @@
+# story = """Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, aperiam eos! Alias voluptate at quisquam, similique
+# minima rerum nulla totam perspiciatis ducimus sequi perferendis aliquid, consectetur corrupti itaque exercitationem
+# accusantium ea adipisci fugit. Laudantium voluptatem modi vel quas voluptates debitis veniam! Tempore ratione esse porro
+# dignissimos totam sint deserunt culpa."""
+# #slicing operations 
+# print(story[::-30])
+#
+
+# note THAT STRIP REMOVES ALL TRAILING CHARACTERS (ORDER DOESN' MATTER
+# e.g "Dasaolu Samuel".strip(" uelSA") outputs 'Dasaolu Sam' while .strip("muelSA") outputs "Dasaolu"
+# text = "My name is Dasaolu Samuel"
+# print(text.replace('samuel', 'Feyigbemiga'))
+
+
+# STRING METHODS
+# text = "Python"
+# print(text.center(20, "-"))  
+# Output: -------Python-------
+
+# TASK 1
+word = "I am a string"
+print(word[::-1])
