be28c5af8901fa3c82e8b57d65b820520aeb63d1
SDasaolu <samueldasaolu40@gmail.com>
2025-08-15 13:18:37 +0100

    task7.3 completed

--- TASKS/Dasaolu_Samuel_task7/task3.py
+++ TASKS/Dasaolu_Samuel_task7/task3.py
@@ -17,4 +17,10 @@ days_tuple = ('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 
 activities_tuple = ('sunday_act', 'monday_act', 'tuesday_act', 'wednesday_act', 'thursday', 'friday')
 zipped_data = zip(days_tuple, activities_tuple)
 activities_pairing = {day: activity for day, activity in zipped_data}
-print("Weird Way: ", activities_pairing)
+days_string = input("Input three days to specify activity for(separated by commas): ").lower()
+user_activity = input('Input activity to specify: ')
+days_to_edit = days_string.split(',')
+for day in days_to_edit:
+  activities_pairing.update({day.strip(): user_activity})
+
+print("Activities Pairing: ", activities_pairing)
