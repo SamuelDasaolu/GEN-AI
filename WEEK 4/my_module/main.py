c24a9d2ddcb1645b15665b2265c3b38cf206d725
SDasaolu <samueldasaolu40@gmail.com>
2025-08-26 12:45:57 +0100

    modules and packages

---
+++ WEEK 4/my_module/main.py
@@ -0,0 +1,44 @@
+# my_module/main.py
+
+import first
+import second
+
+
+# print("")
+# print("=== Math Functions ===")
+# print("5 + 3 =", first.add(5, 3))
+# print("10 - 4 =", first.subtract(10, 4))
+# print("6 * 7 =", first.multiply(6, 7))
+# print("20 / 5 =", first.divide(20, 5))
+
+# print("")
+# my_rhyme = """
+# Black, black, black,
+# The color of the night.
+# Stars come out,
+# And the moon shines bright. 
+
+# Black, black, black,
+# The color of a shoe. 
+# Paint it nice,
+# It looks brand new!
+
+# Black, black, black,
+# The color of a cat. 
+# Soft and quick,
+# What do you think of that?
+# """
+
+# print("My name is Micheal Scoffield. My my code message is: ", second.reverse_string(my_rhyme))
+# print("The total number of characters in my message is: ", second.count_characters(my_rhyme))
+# print("The total number of words in my message is: ", second.count_words(my_rhyme))
+# print("The total number of sentences in my message is: ", second.count_sentences(my_rhyme))
+
+
+# # Lets us the functions in the second.py file
+# print("\n=== String Functions ===")
+# print(second.greet("Sam"))
+# print("Reverse of 'Python' =", second.reverse_string("Python"))
+# print("Character count in sentence =", second.count_characters("Python modules are powerful"))
+# print("Word count in sentence =", second.count_words("Python modules are powerful"))
+
