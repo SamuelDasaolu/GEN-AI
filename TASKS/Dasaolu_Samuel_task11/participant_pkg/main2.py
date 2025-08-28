627e0dc9ae786b2dad40a6e08e6f97524889a8ff
SDasaolu <samueldasaolu40@gmail.com>
2025-08-28 16:03:27 +0100

    Testing task

--- TASKS/Dasaolu_Samuel_task11/participant_pkg/main2.py
+++ TASKS/Dasaolu_Samuel_task11/participant_pkg/main2.py
@@ -9,3 +9,41 @@ allow for multiple participants
 
 """
 
+from pathlib import Path
+
+
+def ask_user(detail, message = ''):
+    return input(f"Participant {detail} ({message if message else ''}): ")
+
+while True: 
+    # Name Collection and Validation
+    while True:
+        name = input("Enter Participant Name: ").title()
+        if name.strip() and len(name.strip().split()) == 2:
+            break
+        print('Name cannot be empty and must be of format: FirstName LastName e.g John Smith')
+        
+        
+    #Age Collection and Validation    
+    while True: 
+        age = input("Enter Participant Age: ")
+        if age and age.isdigit():
+            break
+        print("Age cannot be empty and must be a Whole Number")
+        
+    #Phone Number Collection and Validation:
+    while True:
+        phone_number = input("Enter Phone Number: ")
+        if phone_number:
+            if phone_number.strip().startswith('+') or phone_number.strip().isdigit() and len(phone_number) >= 10:
+                print('Valid phone number')
+                break
+            print("""
+                Invalid Number Entered
+                Phone Number can only be of Forms:
+                0********* or +*************
+                """)
+        print("Phone Number cannot be Empty")
+        
+    print('Outside the loop')
+    break
