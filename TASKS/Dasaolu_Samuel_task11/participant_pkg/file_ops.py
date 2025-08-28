1f4c798b135bca64136741db5172a41b0b954730
SDasaolu <samueldasaolu40@gmail.com>
2025-08-28 21:02:22 +0100

    Created test branch

--- TASKS/Dasaolu_Samuel_task11/participant_pkg/file_ops.py
+++ TASKS/Dasaolu_Samuel_task11/participant_pkg/file_ops.py
@@ -1,15 +1,9 @@
 def save_participant(file_path, participant_dictionary) -> bool:
     """Update A CSV file with a Participants Details 
     Creates A New File if it doesn't exist
-    Creates Header if It Doesnt Exist"""
-    
-    
+    Creates Header if It Doesn't Exist"""
+
+
 def load_participants(file_path) -> list:
     """This reads all participants from the CSV and 
     returns them as a list of dictionaries."""
-    name = enumerate()
-    
-    return name
-    
-    
-    
