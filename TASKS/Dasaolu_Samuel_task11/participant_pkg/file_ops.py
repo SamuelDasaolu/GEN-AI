483b5fb61144738b0f58b3546f8ecec1c2c0bfbd
SDasaolu <samueldasaolu40@gmail.com>
2025-08-28 23:29:39 +0100

    Edited test branch

--- TASKS/Dasaolu_Samuel_task11/participant_pkg/file_ops.py
+++ TASKS/Dasaolu_Samuel_task11/participant_pkg/file_ops.py
@@ -1,9 +1,38 @@
-def save_participant(file_path, participant_dictionary) -> bool:
+import csv
+from pathlib import Path
+
+
+def save_participant(file_path: Path, participant_dictionary: dict):
     """Update A CSV file with a Participants Details 
     Creates A New File if it doesn't exist
     Creates Header if It Doesn't Exist"""
+    try:
+        with open(file_path, 'a', encoding='utf-8', newline='') as csv_file:
+            fieldnames = ['Name', 'Age', 'Phone Number', 'Track']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            if not file_path.exists():  # File is New, update headers
+                writer.writeheader()
+
+            # File Exists, update record
+            writer.writerow(participant_dictionary)
+    except Exception as e:
+        print("An error occurred while saving: ", e)
 
 
-def load_participants(file_path) -> list:
+def load_participants(file_path: Path) -> list:
     """This reads all participants from the CSV and 
     returns them as a list of dictionaries."""
+    participants = []
+    if not file_path.exists(): return participants
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                participants.append(row)
+
+    except Exception as e:
+        print('An error occurred while loading participants: ', e)
+
+    return participants
--- TASKS/Dasaolu_Samuel_task11/participant_pkg/main2.py
+++ TASKS/Dasaolu_Samuel_task11/participant_pkg/main2.py
@@ -11,6 +11,7 @@ allow for multiple participants
 
 from pathlib import Path
 import file_ops as op
+from TASKS.Dasaolu_Samuel_task11.participant_pkg.file_ops import load_participants
 
 
 def ask_user(detail, message=''):
@@ -37,13 +38,13 @@ while True:
     while True:
         phone_number = input("Enter Phone Number: ")
         if phone_number:
-            if phone_number.strip().startswith('+') or phone_number.strip().isdigit() and len(phone_number) >= 10:
-                print('Valid phone number')
+            if (phone_number.strip().startswith('+') or phone_number.strip().isdigit()) and len(phone_number) >= 10:
                 break
             print("""
                 Invalid Number Entered
                 Phone Number can only be of Forms:
                 0********* or +*************
+                And is at least 10 digits
                 """)
         print("Phone Number cannot be Empty")
 
@@ -54,16 +55,35 @@ while True:
             break
         print("Track cannot be empty")
     test_data = {
-        'name': name,
-        'age': age,
-        'phone_number': phone_number,
-        'track': track,
+        'Name': name,
+        'Age': age,
+        'Phone Number': phone_number,
+        'Track': track,
     }
     # Save and Read Participant Data
     workspace_path = Path("WORKSPACE_FILES")
     workspace_path.mkdir(exist_ok=True)
     file_path = workspace_path / 'contacts.csv'
     op.save_participant(file_path, test_data)
+    print(f"Participant {name} saved successfully")
 
-    print('Outside the loop')
+    # while True:
+    save_another = input("Do you want to add another participant? (y/n): ").strip().lower()
+    if save_another != 'y':
+        break
     break
+
+
+print("All Participants details collected successfully \n")
+
+# Load Details
+participants = load_participants(file_path)
+print("\n📋 Final Summary:")
+print(f"Total participants saved: {len(participants)}\n")
+for i, p in enumerate(participants, start=1):
+    name = p.get("Name", "N/A")
+    age = p.get("Age", "N/A")
+    phone = p.get("Phone", "N/A")
+    track = p.get("Track", "N/A")
+
+    print(f"{i}. {name} | Age: {age} | Phone: {phone} | Track: {track}")
