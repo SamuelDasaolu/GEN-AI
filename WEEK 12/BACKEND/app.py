1b207000a76a0b7bfac7fd818182383584d07103
SDasaolu <samueldasaolu40@gmail.com>
2025-10-17 15:43:10 +0100

    End of wk 12

--- WEEK 12/BACKEND/app.py
+++ WEEK 12/BACKEND/app.py
@@ -31,8 +31,32 @@ def get_data():
 @app.post("/create-data")
 def create_data(item: Item):
     data.append(item.model_dump())
-    return {"Message": "Data Record Created", "Data": data}
+    return {"Message": "Data Created", "Data": data}
 
+
+@app.put("/create-record/{id}")
+def create_record(item: Item):
+    data[id] = item.model_dump()
+    return {"Message": "Data Record Added", "Data": data}
+
+
+@app.patch("/update-record/{id}")
+def update_record(id: int, item: Item):
+    # first ensure record exists
+    try:
+        data[id].update(item.model_dump())
+        return {"Message": "Data Record Updated", "Data": data}
+    except IndexError:
+        return {"Message": "Data Record Not Found", "Data": data}
+
+
+@app.delete("/delete-record/{id}")
+def delete_record(id: int):
+    try:
+        del data[id]
+        return {"Message": "Data Record Deleted", "Data": data}
+    except Exception as e:
+        return {"Message": "Data Record Not Found", "Error": e}
 
 if __name__ == "__main__":
     uvicorn.run(app, host=os.getenv("HOST"), port=int(os.getenv("PORT")))
